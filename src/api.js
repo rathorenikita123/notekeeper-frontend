738605e86be33bbf53a0f426dcc760ce6ccc6c28
username <nikitarathore271201@gmail.com>
2022-10-15 13:59:48 +0530

    added backend deplyoed link

--- src/api.js
+++ src/api.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
 export const listNotes = async () => {
-    const data =  await axios.get('http://localhost:8000/notes');
+    const data =  await axios.get('hhttps://notekeeper-backend.herokuapp.com/notes');
     return data;
 }
 
 export const createNote = async (note) => {
-    const data =  await axios.post('http://localhost:8000/notes', note);
+    const data =  await axios.post('https://notekeeper-backend.herokuapp.com/notes', note);
     return data;
 }
 
 export const deleteNote = async (id) => {
-    const data =  await axios.delete(`http://localhost:8000/notes/${id}`);
+    const data =  await axios.delete(`https://notekeeper-backend.herokuapp.com/notes/${id}`);
     return data;
 }
 
 export const updateNote = async (id, note) => {
-    const data =  await axios.put(`http://localhost:8000/notes/${id}`, 
+    const data =  await axios.put(`https://notekeeper-backend.herokuapp.com/notes/${id}`, 
     {
         title: note.updateTitle,
         tagline: note.updateTagline,
@@ -26,7 +26,7 @@ export const updateNote = async (id, note) => {
 }
 
 export const pinNote = async (id, pin) => {
-    const data =  await axios.put(`http://localhost:8000/notes/pin/${id}`, 
+    const data =  await axios.put(`https://notekeeper-backend.herokuapp.com/notes/pin/${id}`, 
     {
         pin
     });
